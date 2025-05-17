f2167b8cf55a1a6e606633c5d5a52a17d9240f92
Hectormd1 <hectormd1990@gmail.com>
2025-05-17 21:04:51 +0200

    fix: video 40 visto

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,16 +1,19 @@
-import colors from 'colors'
-import mongoose from "mongoose";
-import User, {IUSer} from '../models/User';
-
+import colors from "colors"
+import mongoose from "mongoose"
+import User, { IUSer } from "../models/User"
 
 export const connectDB = async () => {
-    try {
-        const {connection} = await mongoose.connect(process.env.MONGO_URI)
-        const url = `${connection.host}:${connection.port}`
-        
-        console.log(colors.magenta.bold(`MONGODB CONECTADO en ${url}`))
-    } catch (error) {
-        console.log(colors.white.bgRed.bold(error.message));
-        process.exit(1)
-    }
+  try {
+    const { connection } = await mongoose.connect(
+      process.env.MONGO_URI
+    )
+    const url = `${connection.host}:${connection.port}`
+
+    console.log(
+      colors.magenta.bold(`MONGODB CONECTADO en ${url}`)
+    )
+  } catch (error) {
+    console.log(colors.white.bgRed.bold(error.message))
+    process.exit(1)
+  }
 }
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,25 @@
-import { Router, Request, Response } from 'express'
-import User from './models/User'
-import { createAccount } from './handlers'
+import { Router, Request, Response } from "express"
+import { body } from "express-validator"
+import { createAccount } from "./handlers"
 
 const router = Router()
 
 // Autenticacion y registro
-router.post('/auth/register', (req: Request, res: Response) => {
-  createAccount(req, res);
-})
+router.post(
+  "/auth/register",
+  body("handle")
+    .notEmpty()
+    .withMessage("El handle no puede ir vacio"),
+  body("name")
+    .notEmpty()
+    .withMessage("El name no puede ir vacio"),
+  body("email").isEmail().withMessage("E-mail no válido"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("El password no puede ir vacio"),
+  (req: Request, res: Response) => {
+    createAccount(req, res)
+  }
+)
 
 export default router
