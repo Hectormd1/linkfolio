eb570723f2a62f2c67de9fa5f0bcc2e6aa09bc8d
Hectormd1 <hectormd1990@gmail.com>
2025-05-26 20:34:31 +0200

    fix: video 51 visto

---
+++ backend/readme.md
@@ -0,0 +1,2 @@
+Full Stack Node.js React TS NestJS Next.js Creando Proyectos
+video 14 completo
---
+++ frontend/src/views/LoginView.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export default function LoginView() {
+  return (
+    <>
+      <h1 className="text-4xl text-white font-blod">Iniciar sesion</h1>
+      <nav className="mt-10"></nav>
+      <nav>
+        <Link
+          className="text-center text-white text-lg block"
+          to="/auth/register"
+        >
+          ¿No tienes cuenta? Crea una aquí.
+        </Link>
+      </nav>
+    </>
+  );
+}
