ee644a62dee47f884d5162543f77e1b24e573c3e
Hectormd1 <hectormd1990@gmail.com>
2025-07-15 01:45:43 +0200

    fix: añadir logs

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -11,7 +11,7 @@ export const corsConfig: CorsOptions = {
     if(whiteList.includes(origin)){
        callback(null, true)
     } else { 
-        callback(new Error('Error de CORS'))
+        // callback(new Error('Error de CORS'))
         
     }
   },
